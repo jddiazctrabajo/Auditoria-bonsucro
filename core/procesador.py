74885ca309de6f61a27a5879bdb00655adac292a
jddiazctrabajo <jenniferdiazgestion@gmail.com>
2026-08-20 11:04:57 -0500

    Update print statement from 'Hello' to 'Goodbye'

--- core/procesador.py
+++ core/procesador.py
@@ -39,7 +39,7 @@ from core.homologacion import Homologador
 from core.maestro import Maestro
 from core.prontuario import Prontuario
 from core.calculos import Calculos
-from core.ciones import dor
+from core.homologacion import Homologador
 
 
 class Procesador:
